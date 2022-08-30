0ac2ea9e62297788d29b52fb2c8287e82b125486
bts5341 <bts5341@psu.edu>
2022-08-29 20:11:09 -0400

    Finshed Learning Loop writing. On to debug!

--- PongAI/Assets/PaddleControl.cs
+++ PongAI/Assets/PaddleControl.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PaddleControl : MonoBehaviour
 {
-    private ArrayList Weights = new ArrayList();
-    private ArrayList OldXVect = new ArrayList();
-    private ArrayList XVect=new ArrayList();
+    private float[] Weights = new float[180];
+    private int[] OldXVect = new int[180];
+    private int[] XVect= new int[180];
     private bool terminal = false;
     private Rigidbody2D rb;
     private float Xpos;
@@ -14,6 +14,8 @@ public class PaddleControl : MonoBehaviour
     private float R = 0;
     private int A = 0;
     private int APrime = 0;
+    private float ALPHA = 0.05f;
+    private float GAMMA = 0.9f;
 
     // Start is called before the first frame update
     void Start()
@@ -32,8 +34,8 @@ public class PaddleControl : MonoBehaviour
     }
     private void FixedUpdate()
     {     
-       while (Ball.GetComponent<Rigidbody2D>().position.y > -5)
-        {
+       //while (Ball.GetComponent<Rigidbody2D>().position.y > -5)
+        //{
             rb.velocity = new Vector3((1 - 2 * A)*5,0,0);//take action
             if(Ball.GetComponent<Rigidbody2D>().position.y > -5)
             {
@@ -46,29 +48,29 @@ public class PaddleControl : MonoBehaviour
             XVect = UpdateStateVect(A);//new (current) State Vector
             if (rb.position.y > -5)
             {
-                //update weights then kill
-                //next episode
+                Weights=UpdateWeights(ALPHA, OldXVect, Weights, R);
+                this.transform.parent.transform.GetComponent<PaddleLord>().Kill();
             }
             APrime = ChooseAction(XVect);
-            //update weights
+            Weights = UpdateWeights(ALPHA, OldXVect, Weights, R,GAMMA,XVect);
             OldXVect = XVect;
             A = APrime;
-        }
+        //}
     }
 
-    public void SetXVect(ArrayList NewXVect)
+    public void SetXVect(int[] NewXVect)
     {
         XVect = NewXVect;
     }
-    public ArrayList GetXVect()
+    public int[] GetXVect()
     {
         return XVect;
     }
-    public void SetWeight(ArrayList NewWeight)
+    public void SetWeight(float[] NewWeight)
     {
         Weights = NewWeight;
     }
-    public ArrayList GetWeight()
+    public float[] GetWeight()
     {
         return Weights;
     }
@@ -84,9 +86,10 @@ public class PaddleControl : MonoBehaviour
     {
         Ball = ball;
     }
-    private ArrayList UpdateStateVect(int Action)
+    private int[] UpdateStateVect(int Action)
     {
-        XVect.Clear();
+        //XVect.Clear();
+        //XVect.ensureCapacity(100);
         Xpos = rb.position.x;
         float BallPosX = Ball.GetComponent<ballcontrol>().ReturnXPos();
         float BallPosY = Ball.GetComponent<ballcontrol>().ReturnYPos();
@@ -98,24 +101,25 @@ public class PaddleControl : MonoBehaviour
         //-5<ball y Pos<5
         //-5<ball x Vel<5
         //-5<ball x Vel<5
-        XVect[(int)((Mathf.RoundToInt(Xpos + 3.66667f) / .98f) - .5f) + 6 * Action] = 1;
-        XVect[(int)((Mathf.RoundToInt(Xpos + 2.9333f) / .98f) - .5f) + 6 * Action + 12] = 1;
-        XVect[(int)((Mathf.RoundToInt(Xpos + 2.2f) / .98f) - .5f) + 6 * Action + 24] = 1;
-        XVect[(int)((Mathf.RoundToInt(BallPosX + 5f) / 1.334f) - .5f) + 6 * Action + 36] = 1;
-        XVect[(int)((Mathf.RoundToInt(BallPosX + 4f) / 1.334f) - .5f) + 6 * Action + 48] = 1;
-        XVect[(int)((Mathf.RoundToInt(BallPosX + 3.0f) / 1.334f) - .5f) + 6 * Action + 60] = 1;
-        XVect[(int)((Mathf.RoundToInt(BallPosY + 8.3333f) / 2.223f) - .5f) + 6 * Action + 72] = 1;
-        XVect[(int)((Mathf.RoundToInt(BallPosY + 6.6665f) / 2.223f) - .5f) + 6 * Action + 84] = 1;
-        XVect[(int)((Mathf.RoundToInt(BallPosY + 5f) / 2.223f) - .5f) + 6 * Action + 96] = 1;
-        XVect[(int)((Mathf.RoundToInt(BallVelX + 8.3333f) / 2.223f) - .5f) + 6 * Action + 108] = 1;
-        XVect[(int)((Mathf.RoundToInt(BallVelX + 6.6665f) / 2.223f) - .5f) + 6 * Action + 120] = 1;
-        XVect[(int)((Mathf.RoundToInt(BallVelX + 5f) / 2.223f) - .5f) + 6 * Action + 132] = 1;
-        XVect[(int)((Mathf.RoundToInt(BallVelY + 8.3333f) / 2.223f) - .5f) + 6 * Action + 144] = 1;
-        XVect[(int)((Mathf.RoundToInt(BallVelX + 6.6665f) / 2.223f) - .5f) + 6 * Action + 156] = 1;
-        XVect[(int)((Mathf.RoundToInt(BallVelX + 5f) / 2.223f) - .5f) + 6 * Action + 168] = 1;
+        Debug.Log(XVect.Length);
+        XVect[(int)(Mathf.RoundToInt(((Xpos + 3.66667f) / .98f) - .5f)) + 6 * Action] = 1;
+        XVect[(int)(Mathf.RoundToInt(((Xpos + 2.9333f) / .98f) - .5f)) + 6 * Action + 12] = 1;
+        XVect[(int)(Mathf.RoundToInt(((Xpos + 2.2f) / .98f) - .5f)) + 6 * Action + 24] = 1;
+        XVect[(int)(Mathf.RoundToInt(((BallPosX + 5f) / 1.334f) - .5f)) + 6 * Action + 36] = 1;
+        XVect[(int)(Mathf.RoundToInt(((BallPosX + 4f) / 1.334f) - .5f)) + 6 * Action + 48] = 1;
+        XVect[(int)(Mathf.RoundToInt(((BallPosX + 3.0f) / 1.334f) - .5f)) + 6 * Action + 60] = 1;
+        XVect[(int)(Mathf.RoundToInt(((BallPosY + 8.3333f) / 2.223f) - .5f)) + 6 * Action + 72] = 1;
+        XVect[(int)(Mathf.RoundToInt(((BallPosY + 6.6665f) / 2.223f) - .5f)) + 6 * Action + 84] = 1;
+        XVect[(int)(Mathf.RoundToInt(((BallPosY + 5f) / 2.223f) - .5f)) + 6 * Action + 96] = 1;
+        XVect[(int)(Mathf.RoundToInt(((BallVelX + 8.3333f) / 2.223f) - .5f)) + 6 * Action + 108] = 1;
+        XVect[(int)(Mathf.RoundToInt(((BallVelX + 6.6665f) / 2.223f) - .5f)) + 6 * Action + 120] = 1;
+        XVect[(int)(Mathf.RoundToInt(((BallVelX + 5f) / 2.223f) - .5f)) + 6 * Action + 132] = 1;
+        XVect[(int)(Mathf.RoundToInt(((BallVelY + 8.3333f) / 2.223f) - .5f)) + 6 * Action + 144] = 1;
+        XVect[(int)(Mathf.RoundToInt(((BallVelX + 6.6665f) / 2.223f) - .5f)) + 6 * Action + 156] = 1;
+        XVect[(int)(Mathf.RoundToInt(((BallVelX + 5f) / 2.223f) - .5f)) + 6 * Action + 168] = 1;
         return XVect;
     }
-    private int ChooseAction(ArrayList StateVector)
+    private int ChooseAction(int[] StateVector)
     {
         
         //0=right
@@ -142,21 +146,40 @@ public class PaddleControl : MonoBehaviour
             return Random.Range(0,2);
         }
     }
-    private float MultiplyVectors(ArrayList A, ArrayList B)
+    private float MultiplyVectors(int[] A, float[] B)
     {
        float total = 0;
-        if (A.Count!=B.Count)
+        if (A.Length!=B.Length)
         {
             return -999;
         }
         else
         {
-            for(int i = 0; i < A.Count; i++)
+            for(int i = 0; i < A.Length; i++)
             {
                 total = total + (float)A[i] * (float)B[i];
             }
             return total;
         }
 
+    }
+    private float[] UpdateWeights(float alpha, int[]  State, float[] Weight, float R)
+    {
+        float q= MultiplyVectors(State, Weight);
+        for (int i=0;i< Weight.Length; i++)
+        {
+            Weight[i] = (float)Weight[i] + alpha * (R - q) * (float)State[i];
+        }
+        return Weight;
+    }
+    private float[] UpdateWeights(float alpha, int[] State, float[] Weight, float R,float gamma, int[] StatePrime)
+    {
+        float q = MultiplyVectors(State, Weight);
+        float qPrime = MultiplyVectors(StatePrime, Weight);
+        for (int i = 0; i < Weight.Length; i++)
+        {
+            Weight[i] = (float)Weight[i] + alpha * (R + gamma*qPrime-q) * (float)State[i];
+        }
+        return Weight;
     }
 }
--- PongAI/Assets/PaddleLord.cs
+++ PongAI/Assets/PaddleLord.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class PaddleLord : MonoBehaviour
 {
     public GameObject PaddleSlave;
-    public ArrayList XVectStore = new ArrayList(180); //3 tilings, with 6 bins, of 5 variables (X paddle, X&Y pos ball, X&Y Vel Ball) times two actions Left or Right
-    public ArrayList WeightStore = new ArrayList(180); //weights 
+   // public int[] XVectStore = new int[180]; //3 tilings, with 6 bins, of 5 variables (X paddle, X&Y pos ball, X&Y Vel Ball) times two actions Left or Right
+    public float[] WeightStore = new float[180]; //weights 
     private GameObject CurrentPaddle;
     public GameObject ball;
     
     // Start is called before the first frame update
     void Start()
     {
-        XVectStore.Clear();
-        WeightStore.Clear();
+        //XVectStore.Clear();
+        //WeightStore.Clear();
+        Birth();
         
         
     }
@@ -35,14 +36,15 @@ public class PaddleLord : MonoBehaviour
 
     public void Kill()
     {
-        XVectStore = CurrentPaddle.GetComponent<PaddleControl>().GetXVect();
+        //XVectStore = CurrentPaddle.GetComponent<PaddleControl>().GetXVect();
         WeightStore = CurrentPaddle.GetComponent<PaddleControl>().GetWeight();
         Destroy(CurrentPaddle);
+        Birth();
     }
     public void Birth()
     {
         CurrentPaddle = Instantiate(PaddleSlave,this.transform);
-        CurrentPaddle.GetComponent<PaddleControl>().SetXVect(XVectStore);
+        //CurrentPaddle.GetComponent<PaddleControl>().SetXVect(XVectStore);
         CurrentPaddle.GetComponent<PaddleControl>().SetBall(ball);
         CurrentPaddle.GetComponent<PaddleControl>().SetWeight(WeightStore);
     }
